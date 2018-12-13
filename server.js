48ccea29822d6928121c6929076b7af22ef6c7ed
Nikhil Singh <30321610+ImSnik@users.noreply.github.com>
2018-12-13 16:10:57 +0530

    Task initialisation

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const hbs = require('hbs');
-const geocode = require('./geocode.js');
-const weather = require('./weather.js');
-const reverse = require('./reverse.js');
 const MongoClient = require('mongodb').MongoClient;
 
 const port = process.env.PORT || 3000;
 
 var app = express();
-app.use(express.static(__dirname + '/views/images') );
+app.use(express.static(__dirname + '/views/styles') );
 app.set('view engine', hbs);
 
 
 app.get('/', (req,res) => {
     res.render('front.hbs');
+});
+
+
+app.get('/quote', (req,res) => {
+    res.render('result.hbs');
 });
